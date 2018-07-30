7f1e734e2be2121363886cdd56fb968b1a66961e
Nate Vack <nate@gimlet.us>
2018-07-30 16:26:31 -0700

    initial commit

---
+++ demean_age.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+age = np.loadtxt("participants.tsv", skiprows=1, usecols=3)
+
+mean_age = sum(age)/len(age)
+
+np.savetxt("demeaned_age.txt", age-mean_age)
+
+print("done!")
