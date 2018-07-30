a80f615bc15a116478c116d3918fc9a055ff5ac7
Nate Vack <nate@gimlet.us>
2018-07-30 16:49:33 -0700

    Add some sanity assertions

--- demean_age.py
+++ demean_age.py
@@ -7,6 +7,9 @@ age = np.loadtxt(sys.argv[1], skiprows=1, usecols=3)
 
 mean_age = sum(age)/len(age)
 
+assert mean_age > 12
+assert mean_age < 100
+
 np.savetxt("demeaned_age.txt", age-mean_age)
 
 print("done!")
